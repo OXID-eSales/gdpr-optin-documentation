f25207446de442c7ee14c33ef0497dc5cfa99d56
Nikola Ivanovski <nikola.ivanovski@dnhsoft.com>
2025-04-14 10:57:42 +0300

    OXDEV-8611 Add version switch for 4.2

--- conf.py
+++ conf.py
@@ -137,7 +137,9 @@ html_context = {
          ('2.3', 'https://docs.oxid-esales.com/modules/gdpr-optin/de/2.3/'),
          ('3.0', 'https://docs.oxid-esales.com/modules/gdpr-optin/de/3.0/'),
          ('4.0', 'https://docs.oxid-esales.com/modules/gdpr-optin/de/4.0/'),
-         ('4.1', 'https://docs.oxid-esales.com/modules/gdpr-optin/de/4.1/')],
+         ('4.1', 'https://docs.oxid-esales.com/modules/gdpr-optin/de/4.1/'),
+         ('4.2', 'https://docs.oxid-esales.com/modules/gdpr-optin/de/4.2/')
+     ],
     'css_files': [('_static/oxid.css')],
     'theme_logo_only': True,
     'show_sphinx': False,
